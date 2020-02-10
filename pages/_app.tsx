9d235cc74261ebc7b8ac617af2ef7317d5cf2c58
Jane Bradley <jane@openlearning.com>
2020-02-10 23:07:11 +1100

    add redux-persist

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,22 @@ import App from "next/app";
 import React from "react";
 import withReduxStore from "../lib/with-redux-store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
 type Props = {
   reduxStore: any;
+  persistor: any;
 };
+
 class MyApp extends App<Props> {
   render() {
-    const { Component, pageProps, reduxStore } = this.props;
+    const { Component, pageProps, reduxStore, persistor } = this.props;
     return (
       <>
         <Provider store={reduxStore}>
-          <Component {...pageProps} />
+          <PersistGate loading={null} persistor={persistor}>
+            <Component {...pageProps} />
+          </PersistGate>{" "}
         </Provider>
       </>
     );
--- store.ts
+++ store.ts
@@ -1,7 +1,18 @@
 import { createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import rootReducer from "./store/index";
 
-export function initializeStore(initialState = {}) {
-  return createStore(rootReducer, initialState, composeWithDevTools());
+const persistConfig = {
+  key: "my-stock-manager",
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export function initializeStore() {
+  const store = createStore(persistedReducer, composeWithDevTools());
+  const persistor = persistStore(store);
+  return { store, persistor };
 }
